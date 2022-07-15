a89fabbe3ad0237dffda3eb57e3fe87301fe908f
Imaad Mohammed <imaadmohammed@gmail.com>
2022-07-14 17:40:13 -0700

    added current tasks card, fixed spacing

--- crud-template/src/App.js
+++ crud-template/src/App.js
@@ -1,12 +1,32 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { AccountCircle, BarChart, Settings } from '@mui/icons-material';
-import { AppBar, Divider, Drawer, IconButton, Menu, Stack, Toolbar, Typography } from '@mui/material';
-import Button from '@mui/material/Button';
-import { Box } from '@mui/system';
-import { CssBaseline } from '@mui/material';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import {
+  AccountCircle,
+  BarChart,
+  HorizontalRule,
+  Settings,
+} from "@mui/icons-material";
+import {
+  AppBar,
+  Divider,
+  Drawer,
+  IconButton,
+  Menu,
+  Paper,
+  Stack,
+  Toolbar,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  CardActions,
+  Checkbox,
+} from "@mui/material";
+import Button from "@mui/material/Button";
+import { Box, Container } from "@mui/system";
+import { CssBaseline } from "@mui/material";
 
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
@@ -33,9 +53,9 @@ function BackendTestBar(props) {
 function Bar(props) {
   return (
     <CssBaseline>
-      <AppBar position='static'>
+      <AppBar>
         <Toolbar>
-          <Typography ml={24} variant="h6" sx={{ flexGrow: 1 }}>
+          <Typography ml={20} variant="h6" sx={{ flexGrow: 1 }}>
             App
           </Typography>
           <IconButton>
@@ -47,34 +67,85 @@ function Bar(props) {
   );
 }
 
-function Nav(props) {
+function SideNav(props) {
   return (
     <Drawer variant="permanent">
-      <Box p={2} width={200} textAlign="center">
-        <Typography p={1} variant='h6'>Navigation</Typography>
-        <Divider variant='middle' />
+      <Box p={2} textAlign="center">
+        <Typography p={1} variant="h6">
+          Navigation
+        </Typography>
+        <Divider variant="middle" />
         <Stack mt={2} spacing={2}>
-          <Button variant="outlined" startIcon={<BarChart />}>Dashboard</Button>
-          <Button variant="outlined" color='secondary' startIcon={<Settings />} >Settings</Button>
+          <Button size="small" variant="outlined" startIcon={<BarChart />}>
+            Dashboard
+          </Button>
+          <Button
+            size="small"
+            variant="outlined"
+            color="secondary"
+            startIcon={<Settings />}
+          >
+            Settings
+          </Button>
         </Stack>
       </Box>
     </Drawer>
   );
 }
+function Tasks(props) {
+  return (
+    <Card sx={{ minWidth: 300, mx: 5, mt: 5 }}>
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 18, pb: 2 }}
+          textAlign="center"
+          variant="subtitle2"
+        >
+          Current Tasks
+        </Typography>
+        <Task />
+        <Task />
+        <Task />
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add Task</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+function Task(props) {
+  return (
+    <Stack direction={"row"} alignItems="center">
+      <Checkbox size="small"></Checkbox>
+      <Typography sx={{ fontSize: 15 }} variant="body2">
+        Example Task
+      </Typography>
+    </Stack>
+  );
+}
+
 function Body(props) {
   return (
-    <Box height={'100vh'} display={'flex'} justifyContent='center' alignItems={'center'}>
-      <BackendTestBar />
-    </Box>
+    <Grid container sx={{ minWidth: 100 }}>
+      <Grid item>
+        <Tasks />
+      </Grid>
+      {/* <Grid item>
+        <Tasks />
+      </Grid> */}
+    </Grid>
   );
 }
 
 function App() {
   return (
     <Box>
-      <Bar />
-      <Nav />
-      <Body/>
+      <SideNav />
+      <Container sx={{ ml: 16, mt: 8 }}>
+        <Bar />
+        <Body />
+      </Container>
     </Box>
   );
 }
